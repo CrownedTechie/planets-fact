531ec74563dcfc60da3cc2e05bbbf9ee65b748bb
Chinenye <149014607+CrownedTechie@users.noreply.github.com>
2025-01-19 17:04:33 +0100

    --style:: make the header responsive on all screens

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,10 +2,13 @@ import hamburgerIcon from "../../assets/hamburger.svg";
 import styles from "./Header.module.css";
 
 export const Header = () => {
- return ( 
-  <header className={styles.header}>
-   <h1 className={`${styles.h1} text-xl`}>the planets</h1>
-   <img src={hamburgerIcon} alt="Hamburger menu" />
-  </header>
+ return (
+  <>
+   <header className={styles.header}>
+    <h1 className={`${styles.h1} text-xl`}>the planets</h1>
+    <img src={hamburgerIcon} alt="Hamburger menu" className={styles.hamburger} />
+   </header>
+   <hr className={styles.hr}/>
+  </> 
  );
 };
