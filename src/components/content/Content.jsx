7da20504264d8a074a23ce8c4c73604e8d66b9ce
Crownedtechie <nenchi00@gmail.com>
2025-02-02 10:30:02 +0100

    --style:: add and style article cards for the content

--- src/components/content/Content.jsx
+++ src/components/content/Content.jsx
@@ -1,27 +1,46 @@
 import React from "react";
-import { NavBar } from "../navBar/NavBar";
 import { Tabs } from "../tabs/Tabs";
 import styles from "./Content.module.css";
+import earthPic from "../../assets/planet-earth.svg";
+import sourceIcon from "../../assets/icon-source.svg";
+import { Article } from "../article/Article";
 
 
 export const Content = () => {
  return ( 
-  <React.Fragment>
-   <nav className={styles.nav}>
-    <ul className={`${styles.ul} text-medium`}> 
-      {/* TABS WILL GO HERE */}
-      <Tabs />
-    </ul>
-   </nav>
+  <div className={styles.contentDiv}>
+    <nav className={styles.nav}>
+      <ul className={`${styles.ul} text-medium`}> 
+        <Tabs />
+      </ul>
+    </nav>
 
-   <figure>
-    {/* PICTURE WILL GO HERE AND BE CHANGING DEPENDING ON THE TAB ABOVE SELECTED */}
-    <img src="" alt="" />
-   </figure>
+    <figure className={styles.figure}>
+      {/* PICTURE WILL GO HERE AND BE CHANGING DEPENDING ON THE TAB ABOVE SELECTED */}
+      <img src={earthPic} alt="Earth" />
+    </figure>
 
-   <div>
-    {/* CONTENT OF THE PLANET GOES HERE */}
-   </div>
-  </React.Fragment>
+    <div className={styles.planetDetailDiv}>
+      {/* CONTENT OF THE PLANET GOES HERE */}
+      <h2 className={`${styles.h2} text-l`}>earth</h2>
+      <p className={`${styles.p} text-small`}>Third planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.</p>
+
+      <p className={styles.source}>
+        <span>Source :</span>
+        <a 
+          href="" 
+          target="_blank" 
+          rel="noopener noreferrer" 
+        >
+            <span>Wikipedia</span>
+            <img src={sourceIcon} alt="" />
+        </a>
+      </p>
+    </div>
+
+    <div>
+      <Article />
+    </div>
+  </div>
  );
 };
--- src/components/tabs/Tabs.jsx
+++ src/components/tabs/Tabs.jsx
@@ -1,19 +1,30 @@
 import styles from "./Tabs.module.css";
-
+import { useMediaQuery } from "../../hooks/useMediaQuery";
 
 export const Tabs = () => {
+  const isLargeScreen = useMediaQuery("(min-width: 601px)");
+
  return ( 
   <>
    <li className={styles.li}>
-    <p className={`${styles.p}`}>overview</p>
+    <p className={`${styles.p}`}> 
+     {isLargeScreen && <span className={styles.span}>01</span>} 
+     overview
+    </p>
    </li>
 
    <li className={styles.li}>
-    <p>structure</p>
+    <p>
+     {isLargeScreen && <span className={styles.span}>02</span>} 
+     structure
+     </p>
    </li>
 
    <li className={styles.li}>
-    <p>surface</p>
+    <p>
+     {isLargeScreen && <span className={styles.span}>03</span>} 
+     surface
+    </p>
    </li>
   </>
  );
